ebf71b78614578122c2cf0ed021a7e8c54613a0f
Mariano Tepper <mariano.tepper@datastax.com>
2025-03-28 15:17:18 -0700

    Fix a bug in the time measurement

--- jvector-examples/src/main/java/io/github/jbellis/jvector/example/Grid.java
+++ jvector-examples/src/main/java/io/github/jbellis/jvector/example/Grid.java
@@ -366,13 +366,14 @@ public class Grid {
         int queryRuns = 2;
         System.out.format("Using %s:%n", cs.index);
         for (var overquery : efSearchOptions) {
-            var start = System.nanoTime();
             int rerankK = (int) (topK * overquery);
             for (var usePruning : usePruningGrid) {
+                var startTime = System.nanoTime();
                 var pqr = performQueries(cs, topK, rerankK, usePruning, queryRuns);
+                var stopTime = System.nanoTime();
                 var recall = ((double) pqr.topKFound) / (queryRuns * cs.ds.queryVectors.size() * topK);
                 System.out.format(" Query top %d/%d recall %.4f in %.2fms after %.2f nodes visited (AVG) with pruning=%b%n",
-                        topK, rerankK, recall, (System.nanoTime() - start) / 1_000_000.0,
+                        topK, rerankK, recall, (stopTime - startTime) / (queryRuns * 1_000_000.0),
                         (double) pqr.nodesVisited / (queryRuns * cs.ds.queryVectors.size()), usePruning);
             }
         }
