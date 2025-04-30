1f7e7cacef853456de933dbe467a82c5528df402
Mariano Tepper <mariano.tepper@datastax.com>
2025-04-30 12:28:01 -0700

    Simplify version tag and check that it is valid

--- jvector-examples/src/main/java/io/github/jbellis/jvector/example/yaml/MultiConfig.java
+++ jvector-examples/src/main/java/io/github/jbellis/jvector/example/yaml/MultiConfig.java
@@ -1,8 +1,21 @@
 package io.github.jbellis.jvector.example.yaml;
 
 public class MultiConfig {
-    public int configVersion;
+    private static int CURRENT_VERSION = 4;
+
+    private int version;
     public String dataset;
     public ConstructionParameters construction;
     public SearchParameters search;
+
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        if (version != CURRENT_VERSION) {
+            throw new IllegalArgumentException("Invalid version: " + version);
+        }
+        this.version = version;
+    }
 }
