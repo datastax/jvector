87a1f3867b81e5e3fc37125f73cb293263a774f2
Mariano Tepper <mariano.tepper@datastax.com>
2025-05-01 08:23:17 -0700

    Remove unneeded imports

--- jvector-examples/src/main/java/io/github/jbellis/jvector/example/yaml/CommonParameters.java
+++ jvector-examples/src/main/java/io/github/jbellis/jvector/example/yaml/CommonParameters.java
@@ -18,14 +18,10 @@ package io.github.jbellis.jvector.example.yaml;
 
 import io.github.jbellis.jvector.example.util.CompressorParameters;
 import io.github.jbellis.jvector.example.util.DataSet;
-import io.github.jbellis.jvector.vector.VectorSimilarityFunction;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
-
-import static io.github.jbellis.jvector.quantization.KMeansPlusPlusClusterer.UNWEIGHTED;
 
 public class CommonParameters {
     public List<Compression> compression;
