32a5d6f150979914a64c87d7226ff4ba53ed3fce
dian-lun-lin <cyc4542000@gmail.com>
2026-03-09 16:55:24 -0700

    support load base vectors only for memory footprint benchmarking

---
+++ jvector-examples/src/main/java/io/github/jbellis/jvector/example/benchmarks/datasets/BaseVectorsDataSet.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright DataStax, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.jbellis.jvector.example.benchmarks.datasets;
+
+import io.github.jbellis.jvector.graph.RandomAccessVectorValues;
+import io.github.jbellis.jvector.vector.VectorSimilarityFunction;
+import io.github.jbellis.jvector.vector.types.VectorFloat;
+import java.util.List;
+
+/**
+ * A lightweight dataset view that exposes only the base vectors needed for build / memory benchmarks.
+ */
+public interface BaseVectorsDataSet {
+    int getDimension();
+    RandomAccessVectorValues getBaseRavv();
+    String getName();
+    VectorSimilarityFunction getSimilarityFunction();
+    List<VectorFloat<?>> getBaseVectors();
+}
